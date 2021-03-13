9311acd0a85486079c72f39b70c94ab61fbd61b6
Your Name,gđ <matejkruhoberec@gmail.com>
2021-03-13 10:05:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pyshorteners
 
 shorthener = pyshorteners.Shortener()
 root = Tk()
-text = Text(root)
+
 root.geometry('400x200')
 
 def button_command():
@@ -15,26 +15,29 @@ def button_command():
 
 def button_command_del():
     entry1.delete(0, END)
-    entry2.delete(0,END)
+    entry2.delete(0, END)
     return None
 
 root.title("URL SHORTHENER")
 
-text.insert(INSERT, "Insert URL here:")
+L1 = Label(root, text="URL goes here:")
 entry1 = Entry(root, width = 60)
+L1.pack()
 entry1.pack()
 
-text.insert(INSERT, "Shortened URL here:")
+
+L2 = Label(root, text="Shorthened URL is here:")
 entry2 = Entry(root, width = 60)
+
+L2.pack()
 entry2.pack()
 
+
 button = Button(root, text="Submit", command = button_command)
-button.pack()
+button.pack(side = LEFT, padx = 100)
 
 button2 = Button(root, text="Delete", command = button_command_del)
-button2.pack()
+button2.pack(side = LEFT)
 
-
-text.pack()
 
 root.mainloop()
